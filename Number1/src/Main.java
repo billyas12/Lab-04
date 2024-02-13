b4656917c63343ac7bbe950a3d1230861cd8d302
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-02-13 14:58:13 -0500

    added comments

--- Number1/src/Main.java
+++ Number1/src/Main.java
@@ -2,13 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //calls printMessage function
         printMessage(5);
 
     }
     public static void printMessage(int n) {
-        while (n > 0) {
-            System.out.println("Hello World");
-            n = n - 1;
+        while (n > 0) {//while loop to ensure number (n) is greater than 0
+            System.out.println("Hello World");//outputs message
+            n = n - 1;//updates n by subtracting 1 before looping again
         }
     }
 }
--- Number2/src/Main.java
+++ Number2/src/Main.java
@@ -3,16 +3,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        //assigns variables
         int n1 = 5;
         int n2 = 55;
         int result = multipleOf7(n1, n2);
 
+        //output message
         System.out.println("The numbers between " +  n1 + " and " +  n2 + " that are multiples of 7 using recursion are " + result);
     }
+   //function to find sum of multiples of 7
     public static int multipleOf7(int n1, int n2){
         if(n1> n2){
             return 0;
         }
+        //finds if n1 modulo 7 = 0 checks if divisble by 7
         if (n1 % 7 == 0){
             return n1 + multipleOf7(n1 + 7, n2);
         }
